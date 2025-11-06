d855ac24022027e2b729082c72e5209c164aa8db
sagarika <elugotisagarika@gmail.com>
2025-11-06 17:04:26 +0530

    adding code

--- interview/listUniqueEle.py
+++ interview/listUniqueEle.py
@@ -1,8 +1,8 @@
-'''#list of unique elements
+#list of unique elements
 
 values=[1,3,2,4,5,7,2,2,3,4,9]
 unique=[]
 for i in values:
     if i not in unique:
         unique.append(i)
-print(unique)'''
+print(unique)
