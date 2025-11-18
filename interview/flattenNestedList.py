37fca5e098f48f3b10af034af66bf9413681d903
sagarika <elugotisagarika@gmail.com>
2025-11-18 12:10:52 +0530

    adding code

--- interview/flattenNestedList.py
+++ interview/flattenNestedList.py
@@ -5,3 +5,44 @@ for subfl in fl1:
     for num in subfl:
         fl2.append(num)
 print(fl2)'''
+
+
+'''listt=[1,2,3,4,5]
+new_one=[]
+for i in listt:
+    new_one.insert(0,i)
+print(new_one)'''
+
+'''lis1=[1,2,3,4,5]
+lis2=[6,7,8,9,10]
+lis1.extend(lis2)
+print(lis1)'''
+
+'''lis=[50,48,36,52,46,32]
+lis.remove(max(lis))
+print("large num",max(lis))'''
+
+'''def function():
+    tuplee=(1,2,3,4,5)
+    sum=0
+    for i in tuplee:
+        sum=sum+i
+    return sum
+    
+obj=function()
+print(obj)'''
+
+'''my_list=[1,2,2,3,5,6,8,2]
+clearing=2
+while clearing in my_list:
+    my_list.remove(clearing)
+print(my_list)'''
+
+'''data="hello everyone!"
+key=3
+print(f"hello {key}everyone!")'''
+
+
+
+
+
